121412a2029bb48061c39811107cb64664449b36
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-26 12:49:16 -0300

    Melhorando comentarios

--- main.c
+++ main.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <locale.h>
 
-// Funcao para mostrar as estatisticas
+// Função para mostrar as estatísticas
 void mostrarEstatisticas(int comparacoes, int trocas, float tempoExecucao)
 {
-	printf("\nNumero de comparacoes: %i\n", comparacoes);
-	printf("nNumero de trocas de posicao: %i\n", trocas);
+	printf("\nNúmero de comparações: %i\n", comparacoes);
+	printf("Número de trocas de posição: %i\n", trocas);
 	printf("Tempo (em segundos): %f\n\n", tempoExecucao);
 }
 
-// Funcao para imprimir o array
+// Função para imprimir o array
 void imprimirArray(int lista[], int tamanho)
 {
 	printf("\n[");
@@ -26,7 +27,7 @@ void imprimirArray(int lista[], int tamanho)
 	printf("]\n");
 }
 
-// Funcao Bubble Sort
+// Função Bubble Sort
 void bubbleSort(int vetor[], int tamanho, int *trocas, int *comparacoes)
 {
 	int aux, i, j;
@@ -46,121 +47,134 @@ void bubbleSort(int vetor[], int tamanho, int *trocas, int *comparacoes)
 	}
 }
 
-// Funcao Merge Sort
+// Função Merge Sort
 void merge(int arr[], int esquerda, int meio, int direita, int *trocas, int *comparacoes)
 {
-    int i, j, k;
-    int n1 = meio - esquerda + 1;
-    int n2 = direita - meio;
+	int i, j, k;
+	int n1 = meio - esquerda + 1;
+	int n2 = direita - meio;
 
-    int L[n1], R[n2];
+	int L[n1], R[n2];
 
-    for (i = 0; i < n1; i++)
-    {
-        L[i] = arr[esquerda + i];
-    }
-    for (j = 0; j < n2; j++)
-    {
-        R[j] = arr[meio + 1 + j];
-    }
+	for (i = 0; i < n1; i++)
+	{
+		L[i] = arr[esquerda + i];
+	}
+	for (j = 0; j < n2; j++)
+	{
+		R[j] = arr[meio + 1 + j];
+	}
 
-    i = 0;
-    j = 0;
-    k = esquerda;
-    while (i < n1 && j < n2)
-    {
-        (*comparacoes)++;
-        if (L[i] <= R[j])
-        {
-            arr[k] = L[i];
-            i++;
-        }
-        else
-        {
-            arr[k] = R[j];
-            j++;
-        }
-        (*trocas)++;
-        k++;
-    }
+	i = 0;
+	j = 0;
+	k = esquerda;
+	while (i < n1 && j < n2)
+	{
+		(*comparacoes)++;
+		if (L[i] <= R[j])
+		{
+			arr[k] = L[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = R[j];
+			j++;
+		}
+		(*trocas)++;
+		k++;
+	}
 
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
+	while (i < n1)
+	{
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
 
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+	while (j < n2)
+	{
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
 }
 
 void mergeSort(int arr[], int esquerda, int direita, int *trocas, int *comparacoes)
 {
-    if (esquerda < direita)
-    {
-        int meio = esquerda + (direita - esquerda) / 2;
-        mergeSort(arr, esquerda, meio, trocas, comparacoes);
-        mergeSort(arr, meio + 1, direita, trocas, comparacoes);
-        merge(arr, esquerda, meio, direita, trocas, comparacoes);
-    }
+	if (esquerda < direita)
+	{
+		int meio = esquerda + (direita - esquerda) / 2;
+		mergeSort(arr, esquerda, meio, trocas, comparacoes);
+		mergeSort(arr, meio + 1, direita, trocas, comparacoes);
+		merge(arr, esquerda, meio, direita, trocas, comparacoes);
+	}
 }
 
-// Funcao QuickSort
+// Função QuickSort
 void quickSort(int *a, int esquerda, int direita, int *trocas, int *comparacoes)
 {
-    int i, j, x, y;
-    i = esquerda;
-    j = direita;
-    x = a[(esquerda + direita) / 2];
-    (*trocas) += 3;
-    while (i <= j)
-    {
-        while (a[i] < x && i < direita)
-        {
-            i++;
-            (*comparacoes)++;
-            (*trocas)++;
-        }
-        while (a[j] > x && j > esquerda)
-        {
-            j--;
-            (*comparacoes)++;
-            (*trocas)++;
-        }
-        if (i <= j)
-        {
-            y = a[i];
-            a[i] = a[j];
-            a[j] = y;
-            (*trocas) += 3;
-            i++;
-            j--;
-        }
-    }
-    if (j > esquerda)
-    {
-        quickSort(a, esquerda, j, trocas, comparacoes);
-    }
-    if (i < direita)
-    {
-        quickSort(a, i, direita, trocas, comparacoes);
-    }
+	int i, j, x, y;
+	i = esquerda;
+	j = direita;
+	x = a[(esquerda + direita) / 2];
+	(*trocas) += 3;
+	while (i <= j)
+	{
+		while (a[i] < x && i < direita)
+		{
+			i++;
+			(*comparacoes)++;
+			(*trocas)++;
+		}
+		while (a[j] > x && j > esquerda)
+		{
+			j--;
+			(*comparacoes)++;
+			(*trocas)++;
+		}
+		if (i <= j)
+		{
+			y = a[i];
+			a[i] = a[j];
+			a[j] = y;
+			(*trocas) += 3;
+			i++;
+			j--;
+		}
+	}
+	if (j > esquerda)
+	{
+		quickSort(a, esquerda, j, trocas, comparacoes);
+	}
+	if (i < direita)
+	{
+		quickSort(a, i, direita, trocas, comparacoes);
+	}
 }
 
+// Função para abrir um arquivo e retornar o ponteiro para ele, com tratamento de erros
+FILE *abrirArquivo(const char *caminho, const char *modo)
+{
+	FILE *arquivo = fopen(caminho, modo);
+	if (arquivo == NULL)
+	{
+		perror("Erro ao abrir o arquivo");
+		exit(EXIT_FAILURE);
+	}
+	return arquivo;
+}
 
 int main()
 {
 	int valor, tamanho;
 	int trocas = 0, comparacoes = 0;
-	int sairPrograma = 0; // Variavel de controle para sair do programa
+	int sairPrograma = 0; // Variável de controle para sair do programa
 
-	printf("\n=== Programa de Analise de Algoritmos de Ordenacao ===\n");
-	printf("Este programa permite analisar o desempenho de algoritmos de ordenacao.\n");
+	setlocale(LC_ALL, "Portuguese");
+
+	printf("\n=== Programa de Análise de Algoritmos de Ordenação ===\n");
+	printf("Este programa permite analisar o desempenho de algoritmos de ordenação.\n");
 
 	while (!sairPrograma)
 	{
@@ -174,42 +188,42 @@ int main()
 		printf("7. 10k\n");
 		printf("8. 50k\n");
 		printf("9. 100k\n\n");
-		printf("Numero do tamanho (1-9): ");
+		printf("Número do tamanho (1-9): ");
 		scanf("%d", &valor);
 
 		char *tamanhos[] = {"10", "50", "100", "500", "1k", "5k", "10k", "50k", "100k"};
 		int tamanhosInteiros[] = {10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000};
 		char *duplicidade[] = {"Com Duplicidade", "Sem Duplicidade"};
-		char *ordem[] = {"Aleat�rio", "Concavo-DecresceCresce", "Convexo-CresceDescresce", "Crescente", "Descrescente"};
+		char *ordem[] = {"Aleatório", "Concavo-DecresceCresce", "Convexo-CresceDescresce", "Crescente", "Descrescente"};
 		char *abreviacoesOrdem[] = {"aleat", "concv", "convx", "cresc", "decre"};
 		char *duplicidadeOrdem[] = {"dup", "uni"};
 
 		printf("\nSelecione a duplicidade:\n\n");
 		printf("1. Com Duplicidade\n");
 		printf("2. Sem Duplicidade\n");
-		printf("\nNumero da duplicidade (1-2): ");
+		printf("\nNúmero da duplicidade (1-2): ");
 		int duplicidadeValor;
 		scanf("%d", &duplicidadeValor);
 
 		if (duplicidadeValor < 1 || duplicidadeValor > 2)
 		{
-			printf("Opcao invalida!\n");
+			printf("Opção inválida!\n");
 			return 1;
 		}
 
 		printf("Selecione a ordem:\n\n");
-		printf("1. Aleatorio\n");
+		printf("1. Aleatório\n");
 		printf("2. Concavo-DecresceCresce\n");
 		printf("3. Convexo-CresceDescresce\n");
 		printf("4. Crescente\n");
 		printf("5. Descrescente\n\n");
-		printf("Numero da ordem (1-5): ");
+		printf("Número da ordem (1-5): ");
 		int ordemValor;
 		scanf("%d", &ordemValor);
 
 		if (ordemValor < 1 || ordemValor > 5)
 		{
-			printf("Opcao invalida!\n");
+			printf("Opção inválida!\n");
 			return 1;
 		}
 
@@ -219,41 +233,35 @@ int main()
 
 		if (arquivoSufixo < 0 || arquivoSufixo > 9)
 		{
-			printf("Sufixo invalido!\n");
+			printf("Sufixo inválido!\n");
 			return 1;
 		}
 
 		tamanho = tamanhosInteiros[valor - 1];
 
-		// Constroi o caminho do arquivo de dados com base nas selecoes do usuario
+		// Constrói o caminho do arquivo de dados com base nas seleções do usuário
 		char caminhoArquivo[256];
 		snprintf(caminhoArquivo, sizeof(caminhoArquivo), "dados\\%s\\%s\\%s\\dt%s%s%s%d.txt",
 				 tamanhos[valor - 1], duplicidade[duplicidadeValor - 1], ordem[ordemValor - 1], abreviacoesOrdem[ordemValor - 1],
 				 tamanhos[valor - 1], duplicidadeOrdem[duplicidadeValor - 1], arquivoSufixo);
 
-		printf("\nBase de dados selecionada selecionada com tamanho %s, %s e %s.\n\n", tamanhos[valor - 1], duplicidade[duplicidadeValor - 1], ordem[ordemValor - 1]);
+		printf("\nBase de dados selecionada com tamanho %s, %s e %s.\n\n", tamanhos[valor - 1], duplicidade[duplicidadeValor - 1], ordem[ordemValor - 1]);
 
 		// Abre o arquivo
-		FILE *arqNome = fopen(caminhoArquivo, "r");
-
-		if (arqNome == NULL)
-		{
-			printf("Erro ao abrir o arquivo. Certifique-se de que o caminho e o nome do arquivo estao corretos.\n");
-			return 1; // Encerra o programa com um codigo de erro
-		}
+		FILE *arqNome = abrirArquivo(caminhoArquivo, "r");
 
 		int lista[tamanho];
 
-		// Variaveis para armazenar tempo
+		// Variáveis para armazenar tempo
 		float beginBubble, endBubble, tempoBubble;
 		float beginMerge, endMerge, tempoMerge;
 		float beginQuick, endQuick, tempoQuick;
 
-		// Verificacao se o arquivo foi devidamente selecionado
+		// Verificação se o arquivo foi devidamente selecionado
 		if (arqNome == NULL)
 		{
-			printf("Erro ao abrir o arquivo. Certifique-se de que o caminho e o nome do arquivo estao corretos.\n");
-			return 1; // Encerra o programa com um codigo de erro
+			printf("Erro ao abrir o arquivo. Certifique-se de que o caminho e o nome do arquivo estão corretos.\n");
+			return 1; // Encerra o programa com um código de erro
 		}
 
 		// O arquivo foi aberto com sucesso, podemos continuar a leitura
@@ -274,26 +282,20 @@ int main()
 		char *nomeDoArquivo = strrchr(caminhoArquivo, '\\');
 		if (nomeDoArquivo != NULL)
 		{
-			nomeDoArquivo++; // Avancar para o primeiro caractere apos a ultima barra invertida
+			nomeDoArquivo++; // Avance para o primeiro caractere após a última barra invertida
 		}
 		else
 		{
-			// Se nao houver barra invertida no caminho, use o caminho inteiro como nome do arquivo
+			// Se não houver barra invertida no caminho, use o caminho inteiro como nome do arquivo
 			nomeDoArquivo = caminhoArquivo;
 		}
 
-		// Adicionar a extensao ".txt" ao nome do arquivo
+		// Adicionar a extensão ".txt" ao nome do arquivo
 		char nomeDoRelatorio[256];
 		snprintf(nomeDoRelatorio, sizeof(nomeDoRelatorio), "relatorios\\relatorio_%s.txt", nomeDoArquivo);
 
-		FILE *relatorio = fopen(nomeDoRelatorio, "w");
-		if (relatorio == NULL)
-		{
-			printf("Erro ao criar o arquivo de relatorio.\n");
-			return 1;
-		}
-
-		fprintf(relatorio, "Relatorio de Analise de Algoritmos de Ordenacao\n\n");
+		FILE *relatorio = abrirArquivo(nomeDoRelatorio, "w");
+		fprintf(relatorio, "Relatório de Análise de Algoritmos de Ordenação\n\n");
 		fprintf(relatorio, "Base de dados selecionada com tamanho %s, %s e %s.\n\n", tamanhos[valor - 1], duplicidade[duplicidadeValor - 1], ordem[ordemValor - 1]);
 		fprintf(relatorio, "Caminho do arquivo: %s\n\n", caminhoArquivo);
 		fclose(relatorio);
@@ -302,15 +304,15 @@ int main()
 
 		while (!sairBaseDados)
 		{
-			printf("Selecione o algoritmo de organizacao:\n");
+			printf("Selecione o algoritmo de organização:\n");
 			printf("1. Bubble Sort;\n");
 			printf("2. Merge Sort;\n");
 			printf("3. Quick Sort;\n");
-			printf("\nNumero do algoritmo (ou 0 para sair da base de dados atual): ");
+			printf("\nNúmero do algoritmo (ou 0 para sair da base de dados atual): ");
 
 			if (scanf("%d", &valor) != 1)
 			{
-				printf("\nEntrada invalida! Insira um numero correspondente a opcao desejada.\n\n");
+				printf("\nEntrada inválida! Insira um número correspondente à opção desejada.\n\n");
 				while (getchar() != '\n')
 					; // Limpa o buffer de entrada
 				continue;
@@ -320,26 +322,20 @@ int main()
 			char *nomeDoArquivo = strrchr(caminhoArquivo, '\\');
 			if (nomeDoArquivo != NULL)
 			{
-				nomeDoArquivo++; // Avance para o primeiro caractere apos a ultima barra invertida
+				nomeDoArquivo++; // Avance para o primeiro caractere após a última barra invertida
 			}
 			else
 			{
-				// Se nao houver barra invertida no caminho, use o caminho inteiro como nome do arquivo
+				// Se não houver barra invertida no caminho, use o caminho inteiro como nome do arquivo
 				nomeDoArquivo = caminhoArquivo;
 			}
 
-			// Adicionar a extensao ".txt" ao nome do arquivo
+			// Adicionar a extensão ".txt" ao nome do arquivo
 			char nomeDoRelatorio[256];
 			snprintf(nomeDoRelatorio, sizeof(nomeDoRelatorio), "relatorios\\relatorio_%s.txt", nomeDoArquivo);
 
-			FILE *relatorio = fopen(nomeDoRelatorio, "a"); // Abra o arquivo de relatorio em modo de anexacao
-			if (relatorio == NULL)
-			{
-				printf("Erro ao criar o arquivo de relatorio.\n");
-				return 1;
-			}
-
-			fprintf(relatorio, "Algoritmo: "); // Indique qual algoritmo esta sendo registrado
+			FILE *relatorio = abrirArquivo(nomeDoRelatorio, "a"); // Abra o arquivo de relatório em modo de anexação
+			fprintf(relatorio, "Algoritmo: ");					  // Indique qual algoritmo está sendo registrado
 			switch (valor)
 			{
 			case 1:
@@ -351,8 +347,8 @@ int main()
 				endBubble = clock();
 				tempoBubble = (float)(endBubble - beginBubble) / CLOCKS_PER_SEC;
 
-				fprintf(relatorio, "Numero de comparacoes: %i\n", comparacoes);
-				fprintf(relatorio, "Numero de trocas de posicao: %i\n", trocas);
+				fprintf(relatorio, "Número de comparações: %i\n", comparacoes);
+				fprintf(relatorio, "Número de trocas de posição: %i\n", trocas);
 				fprintf(relatorio, "Tempo (em segundos): %f\n\n", tempoBubble);
 				mostrarEstatisticas(comparacoes, trocas, tempoBubble);
 
@@ -367,8 +363,8 @@ int main()
 				endMerge = clock();
 				tempoMerge = (float)(endMerge - beginMerge) / CLOCKS_PER_SEC;
 
-				fprintf(relatorio, "Numero de comparacoes: %i\n", comparacoes);
-				fprintf(relatorio, "Numero de trocas de posicao: %i\n", trocas);
+				fprintf(relatorio, "Número de comparações: %i\n", comparacoes);
+				fprintf(relatorio, "Número de trocas de posição: %i\n", trocas);
 				fprintf(relatorio, "Tempo (em segundos): %f\n\n", tempoMerge);
 				mostrarEstatisticas(comparacoes, trocas, tempoMerge);
 				break;
@@ -382,42 +378,42 @@ int main()
 				endQuick = clock();
 				tempoQuick = (float)(endQuick - beginQuick) / CLOCKS_PER_SEC;
 
-				fprintf(relatorio, "Numero de comparacoes: %i\n", comparacoes);
-				fprintf(relatorio, "Numero de trocas de posicao: %i\n", trocas);
+				fprintf(relatorio, "Número de comparações: %i\n", comparacoes);
+				fprintf(relatorio, "Número de trocas de posição: %i\n", trocas);
 				fprintf(relatorio, "Tempo (em segundos): %f\n\n", tempoQuick);
 				mostrarEstatisticas(comparacoes, trocas, tempoQuick);
 
 				break;
 
 			case 0:
-				sairBaseDados = 1; // Define a variavel "sair" como 1 para sair do loop
+				sairBaseDados = 1; // Define a variável "sair" como 1 para sair do loop
 				break;
 
 			default:
-				printf("\nOpcao invalida! Escolha as opcoes listadas (apenas o numero).\n\n");
+				printf("\nOpção inválida! Escolha as opções listadas (apenas o número).\n\n");
 				break;
 			}
 
 			fclose(relatorio);
 
-			// Pergunta ao usuario se deseja selecionar outra base de dados
-			printf("Deseja selecionar outra base de dados? (0 para sair ou qualquer outro numero para continuar): ");
+			// Pergunta ao usuário se deseja selecionar outra base de dados
+			printf("Deseja selecionar outra base de dados? (0 para sair ou qualquer outro número para continuar): ");
 			int resposta;
 			scanf("%d", &resposta);
 			if (resposta == 0)
 			{
-				sairBaseDados = 1; // Define a variavel "sairBaseDados" como 1 para sair do loop
+				sairBaseDados = 1; // Define a variável "sairBaseDados" como 1 para sair do loop
 			}
 		}
 
 		// Fecha o arquivo utilizado
 		fclose(arqNome);
 
-		// Reinicie as variaveis de controle para selecionar uma nova base de dados
+		// Reinicie as variáveis de controle para selecionar uma nova base de dados
 		trocas = 0;
 		comparacoes = 0;
 
-		printf("\nDeseja sair do programa? (0 para sair ou qualquer outro numero para continuar): ");
+		printf("\nDeseja sair do programa? (0 para sair ou qualquer outro número para continuar): ");
 		int resposta;
 		scanf("%d", &resposta);
 		if (resposta == 0)
@@ -426,8 +422,7 @@ int main()
 		}
 	}
 
-	printf("\nObrigado por usar o nosso programa! Esperamos que tenha sido util.\n");
+	printf("\nObrigado por usar o nosso programa! Esperamos que tenha sido útil.\n");
 
 	return 0; // Encerra o programa
 }
-
